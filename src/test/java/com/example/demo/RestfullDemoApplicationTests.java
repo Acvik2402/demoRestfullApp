1cc7f7ca7d880a4bb225de7de7a81a9bbe7b8845
Anton Ponomarev <anton_ponomarev@epam.com>
2021-09-18 08:02:28 +0300

    simple tests added

--- src/test/java/com/example/demo/RestfullDemoApplicationTests.java
+++ src/test/java/com/example/demo/RestfullDemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
+import com.example.demo.users.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest()
 class RestfullDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  MockMvc mockMvc;
 
+  @MockBean
+  private UserService userService;
+
+  @Test
+  void testIncorrectDTOThrowsBadRequestByValidation() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/add-user")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"email\":\"user@server.com\"}"))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest());
+  }
+
+  @Test
+  void testCorrectDTOThrowsBadRequestByValidation() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/add-user")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"userName\":\"Somename\","
+            + "\"email\":\"user@server.com\","
+            + "\"phoneNumber\":\"+79997771177\","
+            + "\"password\":\"12345aaaa\"" +
+            "}"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
 }
